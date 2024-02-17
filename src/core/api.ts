7680384eeaf9b3f1ce05cbcc5646201993e7484b
Khanh Le <masterbruculous@gmail.com>
2024-02-17 20:01:39 +0700

    Update DEBUG flag to false before committing

--- src/core/api.ts
+++ src/core/api.ts
@@ -3,7 +3,9 @@ import {ANDROID_API_BASE_URL, API_BASE_URL, DEV_ANDROID_API_BASE_URL, DEV_API_BA
 import {Platform} from 'react-native';
 
 // Set DEBUG to true to use the DEV_API_BASE_URL and DEV_ANDROID_API_BASE_URL, cancel current react-native server and run it again with yarn start --reset-cache
-export const DEBUG = true;
+// Remember to set DEBUG to false before building the app for production or on real device
+// Always set to false before committing
+export const DEBUG = false;
 
 export const URL = DEBUG ? (Platform.OS === 'android' ? DEV_ANDROID_API_BASE_URL : DEV_API_BASE_URL) : (Platform.OS === 'android' ? ANDROID_API_BASE_URL : API_BASE_URL);
 
